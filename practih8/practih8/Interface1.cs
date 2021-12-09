080b148babc40ce5aee01cfe16092a3fe49548c1
Qu0te3 <90278926+Qu0te3@users.noreply.github.com>
2021-12-09 17:55:18 +0300

    Add files via upload
    Практическая 8

---
+++ practih8/practih8/Interface1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practih8
+{
+    interface ISeries
+    {
+        double Next { get; }
+        double GetNext();
+        void Reset();
+        void SetStart(int startValue);
+    }
+}

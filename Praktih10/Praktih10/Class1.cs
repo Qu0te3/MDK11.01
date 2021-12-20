aefaa47a03c84b6006a2a12162bf551c43b66a60
Morar Vlad ISP-34 <90278926+Qu0te3@users.noreply.github.com>
2021-12-20 20:19:40 +0300

    Практическая (10)
    Практическая (10)

---
+++ Praktih10/Praktih10/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Praktih10
+{
+    internal class NegPosInArray
+    {
+        public (int posCount, int negCount) GetNegPosCount(ArrayList numArray)
+        {
+            int[] array = new int[numArray.Count];
+            numArray.CopyTo(array);
+            int negativeCount = 0, positiveCount = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                    negativeCount++;
+                if (array[i] > 0)
+                    positiveCount++;
+            }
+            return (positiveCount, negativeCount);
+        }
+    }
+}
